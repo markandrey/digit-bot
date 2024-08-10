b6f77b78fd86d3b19bea65ab02186299a51f2395
Mark Andrey <markandreym@gmail.com>
2024-08-10 08:44:06 +0300

    docs(README): added

---
+++ digit_bot/bot.py
@@ -0,0 +1,35 @@
+import os
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+if not TELEGRAM_BOT_TOKEN:
+    exit("Please set TELEGRAM_BOT_TOKEN environment variable")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("Effective chat not found")
+        return
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Привет!")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
